3ca3b3c3af32f82c94f3dba00c1ef12cbb5e41fb
Nick Chapman <nchapman@gmail.com>
2020-06-17 12:34:38 -0700

    Better html email rendering and sizing.

--- components/htmlViewer.tsx
+++ components/htmlViewer.tsx
@@ -1,31 +1,66 @@
 import React from "react";
-import { WebView } from 'react-native-webview'
+import { Dimensions } from "react-native";
+import { WebView } from "react-native-webview";
 
-const injectedJavascript =
-  "setTimeout(function() { window.ReactNativeWebView.postMessage(document.documentElement.offsetHeight) }, 10);true;";
-const injectedCss =
-  "<style>body { font-family: sans-serif; font-size: 16px; }</style>";
+const injectedJavascript = `
+  setTimeout(function() {
+    var height = document.getElementById("bipbopmail").offsetHeight;
+    var width = window.innerWidth;
+    var data = { height: height, width: width }; 
+    window.ReactNativeWebView.postMessage(JSON.stringify(data), 20); 
+  });
+  true;
+`;
+
+const injectedCss = `
+  <style>
+    #bipbopmail {
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"; 
+      font-size: 16px;
+      max-width: 100%;
+    }
+  </style>`;
 
 export default class HtmlViewer extends React.Component {
   state = {
-    height: 0
-  }
+    height: 1000 // Higher starting height seems to lead to better height/width calculations
+  };
+  
   constructor(props) {
     super(props);
   }
 
   handleWebViewMessage(event) {
     if (event.nativeEvent.data) {
-      this.setState({ height: parseInt(event.nativeEvent.data) / 2 });
+      const { height, width } = JSON.parse(event.nativeEvent.data);
+
+      // TODO: pull horizontal padding from styles
+      const zoom = (Dimensions.get("window").width - 40) / width;
+
+      this.setState({ height: (height * zoom) + 20 });
     }
   }
 
+  isComplexHtml(html) {
+    // Obviously should be fanicer but a start
+    return html.indexOf("<table") >= 0;
+  }
+
   prepHtml() {
-    return `<html><head><meta key="viewport" name="viewport" content="width=device-width, initial-scale=0.5, maximum-scale=1" />${injectedCss}</head><body>${this.props.html}</body></html>`;
+    const html = this.props.html;
+
+    return `
+      <!doctype html>
+      <html>
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=${this.isComplexHtml(html) ? 0.5 : 1}" />
+          ${injectedCss}
+        </head>
+        <body><div id="bipbopmail">${html}</div></body>
+      </html>`;
   }
 
   render() {
-    console.log(this.prepHtml());
     return (
       <WebView
         originWhitelist={["*"]}
