ca207ca31024e242598d81d4def9251115304320
Nick Chapman <nchapman@gmail.com>
2020-06-25 12:06:19 -0700

    Added button to expand collapsed quote.

--- components/message/HtmlViewer.js
+++ components/message/HtmlViewer.js
@@ -1,10 +1,10 @@
+import { Button, Icon, useStyleSheet } from "@ui-kitten/components";
 import React, { useState } from "react";
-import { getInjectedCss, injectedJavaScript } from "./injectedContent";
+import { getInjectedCss, getInjectedJavaScript } from "./injectedContent";
 
 import { Dimensions } from "react-native";
 import { WebView } from "react-native-webview";
 import themedStyles from "../../styles";
-import { useStyleSheet } from "@ui-kitten/components";
 
 function calculateHeight(contentHeight, contentWidth) {
   // TODO: pull horizontal padding from styles
@@ -19,29 +19,35 @@ function calculateHeight(contentHeight, contentWidth) {
   return contentHeight * zoom + 20;
 }
 
+function isComplexHtml(html) {
+  return html.indexOf("<table") >= 0;
+}
+
+function getInitialScale(html) {
+  // TODO: This scale should take device screen size into account
+  return isComplexHtml(html) ? 0.5 : 1;
+}
+
+const MoreIcon = (props) => <Icon {...props} name="more-horizontal-outline" />;
+
 function HtmlViewer({ html, hidden = false }) {
   const [height, setHeight] = useState(0);
   const [isReady, setIsReady] = useState(false);
+  const [collapseQuote, setCollapseQuote] = useState(true);
+  const [hasCollapsedQuote, setHasCollapsedQuote] = useState(false);
 
   const styles = useStyleSheet(themedStyles);
   const injectedCss = getInjectedCss(styles);
+  const injectedJavaScript = getInjectedJavaScript(collapseQuote);
 
   function handleWebViewMessage(event) {
-    const { height, width } = JSON.parse(event.nativeEvent.data);
+    const { height, width, collapsedQuote } = JSON.parse(event.nativeEvent.data);
 
     setHeight(calculateHeight(height, width));
+    setHasCollapsedQuote(collapsedQuote);
 
     // We're ready now but wait a beat to prevent flicker
     setTimeout(() => setIsReady(true), 10);
-  }
-
-  function isComplexHtml() {
-    return html.indexOf("<table") >= 0;
-  }
-
-  function getInitialScale() {
-    // TODO: This scale should take device screen size into account
-    return isComplexHtml(html) ? 0.5 : 1;
   }
 
   function prepHtml() {
@@ -54,24 +60,35 @@ function HtmlViewer({ html, hidden = false }) {
           )}" />
           ${injectedCss}
         </head>
-        <body><div id="bipbopmail">${html}</div></body>
+        <body><div id="bipbopmail">${html}</div><script>${injectedJavaScript}</script></body>
       </html>`;
   }
 
   return (
-    <WebView
-      originWhitelist={["*"]}
-      source={{ html: prepHtml() }}
-      onMessage={handleWebViewMessage}
-      injectedJavaScript={injectedJavaScript}
-      scrollEnabled={false}
-      style={{
-        ...styles.htmlViewer,
-        flex: 0,
-        height: hidden ? 0 : height,
-        opacity: isReady ? 1 : 0
-      }}
-    />
+    <>
+      <WebView
+        originWhitelist={["*"]}
+        source={{ html: prepHtml() }}
+        onMessage={handleWebViewMessage}
+        scrollEnabled={false}
+        style={{
+          ...styles.htmlViewer,
+          flex: 0,
+          height: hidden ? 0 : height,
+          opacity: isReady ? 1 : 0
+        }}
+      />
+      {!hidden && hasCollapsedQuote && (
+        <Button
+          appearance="ghost"
+          status="basic"
+          size="small"
+          style={{ width: 20, padding: 0, marginLeft: -5 }}
+          accessoryLeft={MoreIcon}
+          onPress={() => setCollapseQuote(false)}
+        />
+      )}
+    </>
   );
 }
 
--- components/message/injectedContent.js
+++ components/message/injectedContent.js
@@ -1,69 +1,77 @@
-export const injectedJavaScript = `
-  const TEXT_REGEX = /(On [\\s\\S]+ wrote:)/i;
+export function getInjectedJavaScript(collapseQuote = false) {
+  return `
+    const TEXT_REGEX = /(On [\\s\\S]+ wrote:)/i;
 
-  function getDividerNode() {
-    const walker = document.createTreeWalker(
-      document.documentElement,
-      NodeFilter.SHOW_ALL,
-      {
-        acceptNode: function(node) {
-          if (node.nodeName == "#text") {
-            if (!["SCRIPT", "STYLE"].includes(node.parentNode.nodeName)) {
+    function getDividerNode() {
+      const walker = document.createTreeWalker(
+        document.documentElement,
+        NodeFilter.SHOW_ALL,
+        {
+          acceptNode: function(node) {
+            if (node.nodeName == "#text") {
+              if (!["SCRIPT", "STYLE"].includes(node.parentNode.nodeName)) {
+                return NodeFilter.FILTER_ACCEPT;
+              }
+            } else if (['DIV', 'BLOCKQUOTE', 'HR'].includes(node.nodeName)) {
               return NodeFilter.FILTER_ACCEPT;
+            } else {
+              return NodeFilter.FILTER_SKIP;
             }
-          } else if (['DIV', 'BLOCKQUOTE', 'HR'].includes(node.nodeName)) {
-            return NodeFilter.FILTER_ACCEPT;
-          } else {
-            return NodeFilter.FILTER_SKIP;
           }
         }
+      );
+
+      let node, dividerNode;
+
+      while(node = walker.nextNode()) {
+        if (node.nodeName === "#text") {
+          if (node.nodeValue.match(TEXT_REGEX)) {
+            dividerNode = node.parentNode.parentNode;
+          }
+        } else if (node.nodeName === "HR") {
+          if (node.id === "zwchr" || (node.size === "2" && node.width === "100%" && node.align === "center")) {
+            dividerNode = node;
+          }
+        } else if (node.nodeName === "DIV") {
+          if (node.className === "OutlookMessageHeader" || node.className === "gmail_quote") {
+            dividerNode = node;
+          }
+        }
+
+        if (dividerNode) break;
       }
-    );
 
-    let node, dividerNode;
-
-    while(node = walker.nextNode()) {
-      if (node.nodeName === "#text") {
-        if (node.nodeValue.match(TEXT_REGEX)) {
-          dividerNode = node.parentNode.parentNode;
-        }
-      } else if (node.nodeName === "HR") {
-        if (node.id === "zwchr" || (node.size === "2" && node.width === "100%" && node.align === "center")) {
-          dividerNode = node;
-        }
-      } else if (node.nodeName === "DIV") {
-        if (node.className === "OutlookMessageHeader" || node.className === "gmail_quote") {
-          dividerNode = node;
-        }
-      }
-
-      if (dividerNode) break;
+      return dividerNode;
     }
 
-    return dividerNode;
-  }
+    function prune(node) {
+      let parent = node.parentNode;
 
-  function prune(node) {
-    let parent = node.parentNode;
+      while (parent.lastChild !== node) {
+        parent.removeChild(parent.lastChild);
+      }
 
-    while (parent.lastChild !== node) {
-      parent.removeChild(parent.lastChild);
+      parent.removeChild(node);
     }
 
-    parent.removeChild(node);
-  }
+    const collapseQuote = ${collapseQuote};
+    let dividerNode;
 
-  const dividerNode = getDividerNode();
+    if (collapseQuote) {
+      dividerNode = getDividerNode();
 
-  if (dividerNode) {
-    prune(dividerNode);
-  }
+      if (dividerNode) {
+        prune(dividerNode);
+      }
+    }
 
-  let height = document.getElementById("bipbopmail").offsetHeight;
-  let width = window.innerWidth;
-  let data = { height: height, width: width }; 
-  window.ReactNativeWebView.postMessage(JSON.stringify(data));
-  true;`;
+    let height = document.getElementById("bipbopmail").offsetHeight;
+    let width = window.innerWidth;
+
+    let data = { height: height, width: width, collapsedQuote: !!dividerNode }; 
+    window.ReactNativeWebView.postMessage(JSON.stringify(data));
+    true;`
+}
 
 export function getInjectedCss(styles) {
   return `
